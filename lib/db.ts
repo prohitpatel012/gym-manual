8abf694de6ac54cc2389442e0071f00d2d810e4f
prohitpatel012 <prohitpatel.012@gmail.com>
2026-02-22 15:49:31 +0530

    fixed db connection issue

--- lib/db.ts
+++ lib/db.ts
@@ -1,12 +1,10 @@
 import {Pool} from "pg"
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'NextJSBackened',
-    password: 'Choru@1234567890',
-    port: 5432,
- 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 
